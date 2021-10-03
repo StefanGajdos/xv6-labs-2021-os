2f8341aa390c47e4cc78c74cb10a815f99f0edf0
StefanGajdos <xgajdoss1@stuba.sk>
2021-10-03 20:20:31 +0200

    feat: implement sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[]) {
+    int ticks;
+
+    if(argc != 2)
+        fprintf(2,"sleep usage: sleep <ticks>\n");
+
+    ticks = atoi(argv[1]);
+
+    sleep(ticks);
+
+    exit(0);
+}
+
