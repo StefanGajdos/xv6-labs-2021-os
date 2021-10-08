19f26e29e8ececfc9a22977b4c1dfbfcc162ce72
StefanGajdos <xgajdoss1@stuba.sk>
2021-10-08 13:41:45 +0200

    feat: implement trace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// Trace syscalls using mask
+uint64
+sys_trace(void)
+{
+    int mask;
+
+    if(argint(0, &mask) < 0)
+        return -1; // Never reach
+    myproc()->trace_mask = mask; // We setup mask
+    return 0;
+}
